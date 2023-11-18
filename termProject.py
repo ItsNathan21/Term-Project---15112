819f8319a59c63d04b67fbd986fb026f39dba6bf
Nathan Porter <151248086+ItsNathan21@users.noreply.github.com>
2023-11-18 17:06:09 -0500

    Add files via upload

--- termProject.py
+++ termProject.py
@@ -7,11 +7,19 @@ from cmu_graphics import *
 
 def onAppStart(app):
     app.width, app.height = 1800, 900
-    app.playerPosition = (None, None)
-    app.stepsPerSecond = 60
+    app.playerPosition = [app.width/2, app.height/2]
 
 def redrawAll(app):
-    drawRect(50, 50, 50, 50)
+    drawRect(*app.playerPosition, 100, 100, fill ='hotpink', align = 'center')
+
+def onStep(app):
+    enactGravity(app, app.playerPosition)
+
+def enactGravity(app, position):
+    if app.playerPosition[1] + 50 < app.height:
+        app.playerPosition[1] += 10
+
+
 
 
 def main():
